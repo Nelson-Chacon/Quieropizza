5548bb7bcb15c0fd8a7e135920b8c9b5a6b259ea
Nelson Chacon <cursodecomputacion.net@gmail.com>
2021-09-26 16:34:06 -0600

    Agregando cambios

---
+++ Proyecto_Clase/INFINITY_STORE/InfinityStore.Web/Controllers/ProductoController.cs
@@ -0,0 +1,31 @@
+﻿using InfinityStore.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InfinityStore.Web.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+            var producto = new ProductoModel();
+            producto.id = 1;
+            producto.Descripcion = "Iphone X";
+
+            var producto1 = new ProductoModel();
+            producto1.id = 2;
+            producto1.Descripcion = "Xbox 360";
+
+            var ListadeProductos = new List<ProductoModel>();
+
+            ListadeProductos.Add(producto);
+            ListadeProductos.Add(producto1);
+            return View(ListadeProductos);
+        }
+    }
+}
---
+++ Quieropizza/INFINITY_STORE/Comercio_Electronico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Comercio_Electronico.MvcApplication" Language="C#" %>
